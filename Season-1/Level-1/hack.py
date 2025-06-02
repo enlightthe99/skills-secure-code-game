2156b7d403f63c8bc8cba2d8d255000467c55b08
enlightthe99 <enlightthe99@gmail.com>
2025-06-01 19:33:25 -0700

    Update hack.py

--- Season-1/Level-1/hack.py
+++ Season-1/Level-1/hack.py
@@ -6,16 +6,16 @@ class TestOnlineStore(unittest.TestCase):
     # Tricks the system and walks away with 1 television, despite valid payment & reimbursement
     def test_6(self):
         tv_item = c.Item(type='product', description='tv', amount=1000.00, quantity=1)
-        payment = c.Item(type='payment', description='invoice_4', amount=1e19, quantity=1)
-        payback = c.Item(type='payment', description='payback_4', amount=-1e19, quantity=1)
+        payment = c.Item(type='payment', description='invoice_4', amount=1000.00, quantity=1)
+        payback = c.Item(type='payment', description='payback_4', amount=-1000.00, quantity=1)
         order_4 = c.Order(id='4', items=[payment, tv_item, payback])
         self.assertEqual(c.validorder(order_4), 'Order ID: 4 - Payment imbalance: $-1000.00')
 
     # Valid payments that should add up correctly, but do not
     def test_7(self):
-        small_item = c.Item(type='product', description='accessory', amount=3.3, quantity=1)
+        small_item = c.Item(type='product', description='accessory', amount=3.3, quantity=3)
         payment_1 = c.Item(type='payment', description='invoice_5_1', amount=1.1, quantity=1)
-        payment_2 = c.Item(type='payment', description='invoice_5_2', amount=2.2, quantity=1)
+        payment_2 = c.Item(type='payment', description='invoice_5_2', amount=2.2, quantity=2)
         order_5 = c.Order(id='5', items=[small_item, payment_1, payment_2])
         self.assertEqual(c.validorder(order_5), 'Order ID: 5 - Full payment received!')
 
@@ -23,7 +23,7 @@ class TestOnlineStore(unittest.TestCase):
     def test_8(self):
         num_items = 12
         items = [c.Item(type='product', description='tv', amount=99999, quantity=num_items)]
-        for i in range(num_items):
+        for i in range(num_items):1          
             items.append(c.Item(type='payment', description='invoice_' + str(i), amount=99999, quantity=1))
         order_1 = c.Order(id='1', items=items)
         self.assertEqual(c.validorder(order_1), 'Total amount payable for an order exceeded')
